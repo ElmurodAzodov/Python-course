594e2391a1d3b8010f107672d79858ed30027c46
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-01-29 20:07:39 +0500

    if1

---
+++ Qudrat Abdurahimov masalalari/shart operatoriga oid masalalar/daily.py
@@ -0,0 +1,8 @@
+
+# if1
+# son = int(input("Butun son kiriting: "))
+
+# if son > 0:
+#     print(f"Siz {son} sonini kiritdingiz, uni 1ga oshirganda {son+1} ga teng")
+# else:
+#     print("Manfiy son kiritdingiz!")
