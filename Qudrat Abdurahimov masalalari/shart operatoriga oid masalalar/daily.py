6f3b31d2d96e0d2632ff3116133df3902077a288
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-01-29 21:02:36 +0500

    if2

--- Qudrat Abdurahimov masalalari/shart operatoriga oid masalalar/daily.py
+++ Qudrat Abdurahimov masalalari/shart operatoriga oid masalalar/daily.py
@@ -6,3 +6,11 @@
 #     print(f"Siz {son} sonini kiritdingiz, uni 1ga oshirganda {son+1} ga teng")
 # else:
 #     print("Manfiy son kiritdingiz!")
+
+# if2
+son = int(input("Butun son kiriting: "))
+
+if son > 0:
+    print(f"Siz {son} musbat sonini kiritdingiz, uni 1ga oshirganda {son+1} ga teng")
+else:
+    print(f"Siz {son} manfiy sonini kiritdingiz, uni 2ga kamaytirganda {son-2} ga teng")
