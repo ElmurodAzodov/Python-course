d0afec52d1df20c8e94422358a7de94d4eee9e96
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-02-01 23:56:08 +0500

    if22

--- Qudrat Abdurahimov masalalari/shart operatoriga oid masalalar/daily.py
+++ Qudrat Abdurahimov masalalari/shart operatoriga oid masalalar/daily.py
@@ -274,15 +274,28 @@
 
 # if21
 
+# x = int(input("x = "))
+# y = int(input("y = "))
+
+# if x == 0 and y == 0:
+#     print(0)
+# elif x != 0 and y == 0:
+#     print(1)
+# elif x == 0 and y != 0:
+#     print(2)
+# else:
+#     print(3)
+
+# if22
+
 x = int(input("x = "))
 y = int(input("y = "))
 
-if x == 0 and y == 0:
-    print(0)
-elif x != 0 and y == 0:
+if x > 0 and y > 0:
     print(1)
-elif x == 0 and y != 0:
+elif x < 0 and y > 0:
     print(2)
-else:
+elif x < 0 and y < 0:
     print(3)
-
+else:
+    print(4)
