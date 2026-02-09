049043595ae63ab800b203cd509be78f1b9f7b7c
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-02-09 23:16:15 +0500

    list

---
+++ STAGE 6 — Core Data Structures/reference/daily.py
@@ -0,0 +1,49 @@
+# List
+# [...]
+# (...) , {...}
+# my_list = []
+# # print(bool(my_list))
+# numbers = [1, 2, 3, 4, 5]
+# fruits = ["apple", "banana", "cherry"]
+# mixed = [1, "apple", 3.14, True]
+# list_aralash = [True, None, "string", 12, 12.3, KeyError]
+# print(list_aralash)
+# print(numbers)
+# print(mixed)
+
+# ism = list("Elmurod")
+# print(ism)
+# for ozgaruvchi in range(10):
+#     print(ozgaruvchi)
+# l = [ozgaruvchi for ozgaruvchi in range(10)]
+# print(l)
+
+# l = list()
+# for i in range(10):
+#     if i % 2 == 0:
+#         l.append(i)
+
+
+# evens = [i for i in range(10) if i % 2 == 0]
+# print(evens)
+
+
+# l = [2] * 4
+# print(l)
+
+# l = [["apple", "banana"], ["Akbar", "Bobur"], 12, 12.3]
+# print(l[0][1])
+
+# nested = [[1,2,3], 2, 3]
+# nested[0][0] = 9
+# print(nested)
+
+
+# a = []
+# for i in range(10):
+#     a.append(i)
+# print(a)
+
+matrix = [[1, 2, 3], [4, 5, 6, 34, 45, 57], [7, 8, 9, 12, 13]]
+
+print(matrix[2][3])
