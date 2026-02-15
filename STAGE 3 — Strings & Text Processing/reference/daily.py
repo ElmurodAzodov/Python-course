246f4df610445fd5af38d4acf726438d922119b4
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-02-15 22:23:44 +0500

    .

---
+++ STAGE 3 — Strings & Text Processing/reference/daily.py
@@ -0,0 +1,65 @@
+
+text = "f123 qWe, HelLo g,' woRld!@123"
+# # print(text[:2]) # [start:stop-1:step]
+# # print(text[2:8])
+# # print(text[2:10:2])
+# # print(text[::2])
+# print(text[::-1])
+# n = text.replace("W", "V")
+# print(n)
+# print(text)
+# 1
+# print("qwe qwe qew qwe ".upper())
+# print(text.upper())
+# 2
+# print(text.lower())
+# 3
+# print(text.capitalize())
+# 4
+# print(text.title())
+# 5
+# print(text.swapcase())
+# 6
+# print(text.find("w"))
+# print(text.find("i"))
+# 7
+# print(text.index("i"))
+# 8
+# print(text.replace("qWe ", "O'zgartirilsin"))
+# 9, 10, 11
+# print(text.strip()) # .lstrip(), .rstrip()
+# 12
+# print(text.split("@"))
+# print(len(text.split("@")))
+# 13
+# print("text".join(['-', '*', 'a', '999', '7777', 777]))
+# 14
+# print("Hello".isalpha())
+# 15
+# print("00000".isdigit())
+# 16
+# print("Hello123".isalnum())
+# 17
+# print(" ".isspace())
+# 18
+# print("Hello World".startswith("Hello "))
+# 19
+# print("Hello World".endswith("ld"))
+# 20
+# print(f"Chaqirildi {text}")
+# print("Chaqirildi {}".format(text))
+text = "   hello python world   "
+# 1. Boshi va oxiridagi bo‘shliqni olib tashlash
+clean = text.strip()
+print(clean)
+# 2. Katta harflar
+upper_text = clean.upper()
+print(upper_text)
+# 3. So‘zlarni ajratish
+words = upper_text.split()
+print(words)
+# 4. Birinchi so‘zni almashtirish
+words[0] = "HI"
+# 5. Ro‘yxatni stringga birlashtirish
+final_text = " ".join(words)
+print(final_text)
