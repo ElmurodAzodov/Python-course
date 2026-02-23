25d1d3c41fd1dd1271e18a8557bee7a05099d495
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-02-23 22:50:53 +0500

    .

--- STAGE 3 — Strings & Text Processing/topshiriq/string_practice.py
+++ STAGE 3 — Strings & Text Processing/topshiriq/string_practice.py
@@ -6,7 +6,6 @@ String Methods • Searching • Replacing
 ===========================================================
 """
 
-
 # =========================================================
 # 1–50: STRING CREATION, INDEXING, METHODS VA AMALIYOT
 # =========================================================
@@ -202,14 +201,18 @@ String Methods • Searching • Replacing
 
 # 29. Palindrom ekanligini tekshir.
 
-word = "radar"
-if word == word[::-1]:
-    print(True)
-else:
-    print(False)
+# word = "radar"
+# if word == word[::-1]:
+#     print(True)
+# else:
+#     print(False)
 
 
 # 30. Matndagi so‘zlar sonini aniqlang.
+
+text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem."
+print(len(text.split()))
+
 # 31. Eng uzun so‘zni top.
 # 32. Eng qisqa so‘zni top.
 # 33. Katta harflar sonini aniqlang.
