5810b5a11942dc74a252820c3f3561d7f5497d43
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-03-14 22:45:59 +0500

    add: lesson

---
+++ STAGE 8 — Advanced Functions & Functional Concepts/reference/daily.py
@@ -0,0 +1,63 @@
+# Stage 8
+
+# def greet(name=0):
+#     return f"Hello {name}"
+
+# say_hello = greet
+
+# print(say_hello("Ali")) #greet("Ali")
+# print(type(say_hello))
+
+# print(greet)
+
+# ---------------------------------------------
+
+# def sum(a, b):
+#     return a + b
+# print(sum(1, 2))
+
+# sum = lambda a, b: a + b
+# print(type(sum(1, 2)))
+
+# print((lambda a, b: a + b)(1, 2))
+# print(type((lambda a, b: a + b)(1, 2)))
+
+# kv = lambda parametr: parametr ** 2
+# print(kv(4))
+
+# lmb = lambda: 3 + 4
+# print(lmb())
+
+# check = lambda x: "Even" if x % 2 == 0 else "Odd"
+
+# print(check(10))
+# print(check(7))
+
+
+# ^ map()
+
+# l = [2, 3, 4, 5, 6]
+
+# def kub(a):
+#     return a ** 3
+
+# natija = list(map(kub, l))
+# print(natija)
+
+# natija = list(map(lambda a: a ** 3, l))
+# print(natija)
+
+# -------------------------------------------------
+
+# names = ["ali", "vali", "hasan"]
+# result = list(map(str.upper, names))
+# print(result)
+
+
+# numbers = ["1", "2", "3", "4"]
+# result1 = list(map(int, numbers))
+# print(result1)
+
+# ------------------------------------------
+
+
