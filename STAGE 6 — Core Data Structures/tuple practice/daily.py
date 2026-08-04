c65581688b5e960477163194bf1d632a03c12815
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-04 16:27:01 +0500

    add

--- STAGE 6 — Core Data Structures/tuple practice/daily.py
+++ STAGE 6 — Core Data Structures/tuple practice/daily.py
@@ -1,9 +1,28 @@
-sonlar = (1, 2, 3, 4, 5)
-aralash = ("Elmurod", 24, True, False, None, 2+4)
-son = 2.5
-print(sonlar, son, aralash)
-print(type(sonlar), type(son), type(aralash))
+# sonlar = (1, 2, 3, 4, 5)
+# aralash = ("Elmurod", 24, True, False, None, 2+4)
+# son = 2.5
+# print(sonlar, son, aralash)
+# print(type(sonlar), type(son), type(aralash))
 
+#*______________________________________________________________________
 
+# my_tuple = (
+#     1,
+#     2,
+#     3,
+#     "Elmurod",
+#     "Dasturchi",
+#     True,
+#     False,
+#     (1, 2, 3, 4, 5),
+#     [1, 2, 3, 4, 5],
+#     {"ism": "Elmurod", "mutaxassisligi": "Dasturiy injiniring",},
+#     None
+# )
+# print(my_tuple)
+# print(type(my_tuple))
 
+#*______________________________________________________________________
 
+# sonlar = (2,)
+# print(sonlar, type(sonlar))
