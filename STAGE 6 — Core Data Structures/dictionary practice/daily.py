8cb97625980468dd427c527e169c203d3fd49e37
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-15 17:58:47 +0500

    add

---
+++ STAGE 6 — Core Data Structures/dictionary practice/daily.py
@@ -0,0 +1,34 @@
+lugat = {
+    "one": 1,
+    1: "text-1",
+    (1,2,3): 12,
+    12.5: 12.5,
+    
+}
+
+# print(lugat, type(lugat))
+
+
+lugat1 = dict(
+    son1 = 1, son2 = 2, son3 = 3, son4 = 4
+)
+# print(lugat1, type(lugat1))
+
+# print(lugat1.keys())
+# print(lugat1.values())
+# print(lugat1.items())
+
+
+
+oquvchi = {
+    "ismi": "Jahongir",
+    "familiyasi": "Erkinov",
+    "yoshi": 14,
+    "JSHSHR": 40506070809012,
+    "maktab": "1-son maktabi",
+    "tuman": "Hazorasp",
+    "mahalla": "Temirchi mahallasi",
+}
+
+print(oquvchi)
+print(oquvchi.items())
