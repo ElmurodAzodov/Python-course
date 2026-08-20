73f946560457e52ce8f56f08bc838b19656d02ad
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-20 17:31:21 +0500

    add

---
+++ STAGE 7 — Functions & Scope/daily1.py
@@ -0,0 +1,41 @@
+
+# def salom_teksti():
+#     print("Assalomu alaykum")
+
+# salom_teksti()
+#* ================================================
+# def salom_teksti(soz, gap):
+#     print(soz, gap)
+
+# salom_teksti("salom", "Assalomu alaykum")
+#* ================================================
+
+# def sonlar_yigindisi(son_1, son_2):
+#     # print(son_1 + son_2)
+#     print("Salom")
+#     return son_1 + son_2
+
+# # sonlar_yigindisi(2, 3)
+# print(sonlar_yigindisi(2, 3))
+
+
+#* ================================================
+
+# def returnni_ishlatmasak():
+#     print("Returnsiz")
+#     return ""
+# # returnni_ishlatmasak()
+
+
+# print(returnni_ishlatmasak())
+
+#* ================================================
+
+
+def qiymat_kiriting(a):
+    return a, type(a)
+
+qiymat = input("Kiriting: ")
+
+print(qiymat_kiriting(qiymat))
+
